90f5311f6e6d712afdd95ac09b0571e38964aec3
Shobha Goudar <shobhavgoudar4@gmail.com>
2025-11-26 19:01:09 +0530

    Login Part Done

--- src/main/java/com/jsp/book_my_ticket/util/AdminRegistration.java
+++ src/main/java/com/jsp/book_my_ticket/util/AdminRegistration.java
@@ -15,7 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class AdminRegistration implements CommandLineRunner {
 
-	@Value("${admin.email)")
+	@Value("${admin.email}")
 	private String email;
 	
 	@Value("${admin.password}")
